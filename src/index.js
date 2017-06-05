9d098441397c43354dc5da334977994ef43f2ba2
Rachna Sidana <rachnasidana28feb@gmail.com>
2017-06-05 00:13:41 -0500

    simple hello world

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// A functional component
+
+const App = () => {
+  return (
+    <h1> Hello World ! </h1>
+  );
+};
+
+
+ReactDOM.render(<App />, document.querySelector('.container'))
