dd457010f32bd16ba3c934c373979236e922b8b9
Rachna Sidana <rachnasidana28feb@gmail.com>
2017-06-05 01:28:25 -0500

    search bar with state and youtube api static search

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar.js';
+import YTSearch from 'youtube-api-search';
 
+const API_KEY ='XXX';
+
+
+YTSearch({key:API_KEY, term:'Ed Sheeran'}, function(data){
+  alert(data);
+  console.log(data);
+});
 // A functional component
 
 const App = () => {
   return (
-    <h1> Hello World ! </h1>
+    <div>
+      <SearchBar />
+    </div>
   );
 };
 
