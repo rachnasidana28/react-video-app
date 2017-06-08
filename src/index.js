5b7a50564bad7754f4ff4ef6fd14fd353360130c
Rachna Sidana <rachnasidana28feb@gmail.com>
2017-06-08 00:42:54 -0500

    implement video player with video details

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar.js';
 import VideoList from './components/video_list.js';
+import VideoDetail from './components/video_detail';
 
 import YTSearch from 'youtube-api-search';
 
@@ -23,6 +24,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
         <VideoList videos = {this.state.videos} />
       </div>
     );
